8e06b79bc6a29bced4a2fc014842b99bb1069489
AditiBansal <aditibansal22@gmail.com>
2021-10-14 15:32:47 -0400

    Seacrh by categories

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -3,14 +3,20 @@ import { getProductsByCount, fetchProductsByFilter } from '../functions/product'
 import {useSelector, useDispatch, createSelectorHook} from 'react-redux';
 import ProductCard from '../components/cards/ProductCard';
 import ProductListItems from '../components/cards/ProductListItems';
-import {Menu, Slider} from 'antd';
-import { DollarOutlined } from '@ant-design/icons';
+import {Menu, Slider,Checkbox} from 'antd';
+import { DollarOutlined,DownSquareOutlined } from '@ant-design/icons';
+import {getCategories} from '../functions/category'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
     const [price,setPrice] =useState([0,0]);
     const [ok,setOk] = useState(false);
+    //to show all categories option
+    const [categories,setCategories] = useState([]);
+    //to show the selected categories
+    const [categoryIds,setCategoryIds] = useState([]);
+
     let {search} = useSelector((state) => ({...state}));
     const {text} = search;
     const {SubMenu, ItemGroup} = Menu;
@@ -19,9 +25,12 @@ const Shop = () => {
      // load products on pageload by default
     useEffect(() => {
         loadAllProducts();
+        //fetch categories
+        getCategories().then((res) => setCategories(res.data));
     },[])
 
     const fetchProducts = (args) => {
+        console.log(args);
         fetchProductsByFilter(args)
        .then((res) => {
            setProducts(res.data);
@@ -60,9 +69,32 @@ const Shop = () => {
             payload: {text:""}
         });
         setPrice(value);
+        //To clear category
+       // setCategoryIds([]);
         setTimeout(() =>{setOk(!ok)},300);
     }
 
+    //based on categories
+    const showCategories = () => categories.map((c) => <div key={c._id}>
+                <Checkbox className="pb-2 pl-4 pr-4" value={c._id} name="category" onChange={handleCheck} checked={categoryIds.includes(c._id)}>{c.name}</Checkbox>
+                <br />
+    </div>)
+    const handleCheck = (e) =>
+    {
+        //console.log(e.target.value);
+        // dispatch({
+        //     type:"SEARCH_QUERY",
+        //     payload: {text:""}
+        // });
+        // setPrice([0,0]);
+        let inTheState = [...categoryIds];
+        let justChecked = e.target.value;
+        let foundInTheState = inTheState.indexOf(justChecked) === -1 ? inTheState.push(justChecked) : inTheState.splice(inTheState.indexOf(justChecked),1);
+
+        setCategoryIds(inTheState);
+        //console.log(inTheState);
+        fetchProducts({category: inTheState});
+    }
     return (<div className="container-fluid">
         <div className="row">
             <div className="col-md-3 pt-2">
@@ -73,7 +105,11 @@ const Shop = () => {
                         <div>
                             <Slider className="ml-4 mr-4" tipFormatter = {(v) => `$${v}`} range calue={price} onChange={handleSlider} max ="9999"/>
                         </div>
-
+                    </SubMenu>
+                    <SubMenu key='2' title={<span className="h6"> <DownSquareOutlined />Categories</span>} >
+                        <div style={{marginTop:"-10px"}}>
+                            {showCategories()}
+                        </div>
                     </SubMenu>
                 </Menu>
             </div>
